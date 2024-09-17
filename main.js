e04f5e19c10bee42c0b6b2ae890835c123d68be7
Rafael Francisco Serrano Urtecho <rafa.tesla@gmail.com>
2024-09-17 04:38:50 -0700

    formulario html de contacto

--- main.js
+++ main.js
@@ -8,16 +8,16 @@ const body = document.body;
 const icon = darkModeToggle.querySelector('i');
 
 darkModeToggle.addEventListener('click',
-    ()=>{
+    () => {
         // cambiamos de clase al cuerpo
         body.classList.toggle('dark-mode');
 
-        if(body.classList.contains('dark-mode')){
+        if (body.classList.contains('dark-mode')) {
             icon.classList.remove('fa-moon');
             icon.classList.add('fa-sun');
-        }else{
+        } else {
             icon.classList.remove('fa-sun');
-            icon.classList.add('fa-moon');            
+            icon.classList.add('fa-moon');
         }
     }
 )
